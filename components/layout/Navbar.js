de92887faed354f456be2c7529714a6801151f30
Federico Gentili <federicogentili05@gmail.com>
2026-02-09 20:57:36 +0100

    feat: add logo to Navbar and update ClickSpark z-index for improved interaction

--- components/layout/Navbar.js
+++ components/layout/Navbar.js
@@ -2,6 +2,8 @@
 import { motion, useScroll, useMotionValueEvent } from "framer-motion"
 import { LanguageSwitcher } from "../ui/LanguageSwitcher"
 import { useState } from "react"
+import logo from "@/public/icon.svg"
+import Image from "next/image"
 
 export default function Navbar() {
   const { scrollY } = useScroll()
@@ -29,7 +31,9 @@ export default function Navbar() {
     >
       <div className="flex items-center justify-between max-w-7xl mx-auto p-5">
         <div className="flex items-center justify-center w-fit gap-5">
-          <div>LOGO</div>
+          <div>
+            <Image src={logo} alt="Federico Gentili" className="logo size-7" />
+          </div>
           <div className="h-6 w-px bg-linear-to-b from-foreground/5 via-foreground/50 to-foreground/5 rounded-full"></div>
           <div>LINKS</div>
         </div>
