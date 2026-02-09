9ea41ed6243a1772decc1c6ef6daaac8983e76c6
Federico Gentili <federicogentili05@gmail.com>
2026-02-09 23:30:11 +0100

    feat: update NotFound component to use full screen height for better visibility

--- app/not-found.js
+++ app/not-found.js
@@ -3,7 +3,7 @@ import Link from "next/link"
 
 export default function NotFound() {
   return (
-    <div>
+    <div className="h-screen">
       <h2>Not Found</h2>
       <p>Could not find requested resource</p>
       <Link href="/">
