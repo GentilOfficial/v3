74c22764cedd89578d79bdb13a8dbf5fd4cd0f36
Federico Gentili <federicogentili05@gmail.com>
2026-02-09 19:16:35 +0100

    refactor: remove unused UI components and add DarkVeil effect
    - Deleted DotPattern, Globe, GridPattern, InteractiveGridPattern, LightRays, and Pointer components as they are no longer needed.
    - Introduced DarkVeil component for enhanced visual effects using OGL for rendering.
    - Updated package dependencies to include gsap, ogl, and three.js for new animations and graphics capabilities.

--- app/components/layout/Navbar.js
+++ app/components/layout/Navbar.js
@@ -2,7 +2,7 @@ import { LanguageSwitcher } from "../ui/LanguageSwitcher"
 
 export default function Navbar() {
   return (
-    <nav className=" w-full backdrop-blur-md shadow-md border-b border-gray-200">
+    <nav className=" w-full backdrop-blur-lg bg-background/50 border-b border-foreground/5">
       <div className="flex items-center justify-between max-w-7xl mx-auto p-5">
         <div className="flex items-center justify-center w-fit gap-5">
           <div>LOGO</div>
--- app/layout.js
+++ app/layout.js
@@ -1,10 +1,7 @@
 import "./globals.css"
 import { LanguageProvider } from "@/app/providers/LanguageContext"
 import Navbar from "./components/layout/Navbar"
-import { GridPattern } from "@/components/ui/grid-pattern"
-import { cn } from "@/lib/utils"
-// import { Pointer } from "@/components/ui/pointer"
-// import { LightRays } from "@/components/ui/light-rays"
+import DarkVeil from "@/components/ui/DarkVeil"
 
 export const metadata = {
   title: "Federico Gentili",
@@ -14,16 +11,15 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="dark">
-        <GridPattern
-          width={30}
-          height={30}
-          x={-1}
-          y={-1}
-          strokeDasharray={"4 2"}
-          className={cn(
-            "mask-[radial-gradient(300px_circle_at_center,white,transparent)]",
-          )}
+      <body className="dark relative">
+        <DarkVeil
+          hueShift={25}
+          noiseIntensity={0}
+          scanlineIntensity={0}
+          speed={1.2}
+          scanlineFrequency={0}
+          warpAmount={1}
+          resolutionScale={1}
         />
         <LanguageProvider>
           <Navbar />
